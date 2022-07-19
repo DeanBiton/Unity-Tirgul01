bc0273d503d19342d17eaca31288b80eb26dff43
Dean <dinbiton100@gmail.com>
2022-07-19 22:47:39 +0300

    add control buttons

--- Tirgul01/Assets/Scripts/MyCharacterController.cs
+++ Tirgul01/Assets/Scripts/MyCharacterController.cs
@@ -14,10 +14,24 @@ public class MyCharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
-            gameObject.transform.position = new Vector3(0,0,1f);
+        checkControlButtons();
+    }
 
-        //myTrans.RotateAround(myTrans.position,Vector3.right,5f);
-        //gameObject.GetComponent<Rigidbody>().AddForce(5f*Vector3.up);
+    void checkControlButtons()
+    {
+        if(Input.GetKey(KeyCode.UpArrow))
+            transform.position += new Vector3(0,0,0.01f);
+        if(Input.GetKey(KeyCode.DownArrow))
+            transform.position += new Vector3(0,0,-0.01f);
+        if(Input.GetKey(KeyCode.RightArrow))
+            transform.position += new Vector3(0.01f,0,0);
+        if(Input.GetKey(KeyCode.LeftArrow))
+            transform.position += new Vector3(-0.01f,0,0);  
+        if(Input.GetAxis("Mouse X")<0)
+            transform.RotateAround(transform.position,Vector3.up,-1f);
+        if(Input.GetAxis("Mouse X")>0)
+            transform.RotateAround(transform.position,Vector3.up,1f);
+        if(Input.GetKey(KeyCode.Space))
+            gameObject.GetComponent<Rigidbody>().AddForce(2f*Vector3.up);
     }
 }
